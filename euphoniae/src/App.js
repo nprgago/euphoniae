691caf6c57ea46703005e3d5e8c7dd685e39c075
Nuno Pedro Ribeiro Gago <nprgago@gmail.com>
2018-05-22 16:41:52 +0100

    feat: add navbar to app and it style

--- euphoniae/src/App.js
+++ euphoniae/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar } from 'react-bootstrap';
 import logo from './logo.svg';
 import './scss/App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App Container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to='/'>
+                <img alt='logo' src={ logo } className='logo pull-left'/>
+                <span className='title pull-left'>Euphoniae</span>
+              </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle/>
+          </Navbar.Header>
+        </Navbar>
+        <hr/>  
       </div>
     );
   }
