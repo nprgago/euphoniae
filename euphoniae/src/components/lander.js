0193584f7d5b6c81fb92d6310b23966b25b40452
Nuno Pedro Ribeiro Gago <nprgago@gmail.com>
2018-05-24 00:21:12 +0100

    fix: changes on land page

--- euphoniae/src/components/lander.js
+++ euphoniae/src/components/lander.js
@@ -1,81 +1,46 @@
 import React, { Component } from 'react';
-import { Carousel, Glyphicon } from 'react-bootstrap';
+import CountUp from 'react-countup';
+import { Link } from 'react-router-dom';
 import '../scss/lander.scss';
 
 
 class Lander extends Component {
 	
-	importAll = (r) => {
-		let images = {};
-		r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
-		return images;
-	}
+	
 
 	render() {
 		
-		const images = this.importAll(require.context('../img', false, /\.(png|jpe?g|svg)$/));
 		
 		return(
 			<div className='lander'>
-		        
-		        <div className= 'carrousel-container'>
-		          <Carousel>
-		            
-		            <Carousel.Item>
-		              <img width={900} height={500} alt="Girl listening music" src={images['carrousel-1.jpg']}/>
-		              <Carousel.Caption>
-		                <h3>Song Addicted</h3>
-		                <p>Find the lastest and up to date songs in Euphoniae </p>
-		              </Carousel.Caption>
-		            </Carousel.Item>
-		            
-		            <Carousel.Item>
-		              <img width={900} height={500} alt="Girl listening music" src={images['carrousel-2.jpg']} />
-		              <Carousel.Caption>
-		                <h3>Favorite Songs</h3>
-		                <p>Add your best song to your favorites</p>
-		              </Carousel.Caption>
-		            </Carousel.Item>
-		            
-		            <Carousel.Item>
-		              <img width={900} height={500} alt="Girl listening music" src={images['carrousel-3.jpg']} />
-		              <Carousel.Caption>
-		                <h3>Star Now</h3>
-		                <p>Register to start listing your songs </p>
-		              </Carousel.Caption>
-		            </Carousel.Item>
-		          
-		          </Carousel>
-		        </div>		    
+		       	
+		       	<h1>More Music!</h1>
+		       	<h2>Discover the lastest songs of your favorites artists.</h2>
+		       	
+		       	<Link to='/signup'>
+					<p> Signup </p>
+				</Link>
 
-		        <div className='info'>
-		          
-		          <span>
-		            <Glyphicon glyph="plus" className='btn-lg'/>
-		            <h3> 100 Songs </h3>
-		            <p> Access to lastes song hits </p> 
-		          </span>
-		          
-		          <span>
-		            <Glyphicon glyph="heart" className='btn-lg'/>
-		            <h3> Add to Favorites </h3>
-		            <p> List of your favorites </p>
-		          </span>
-		          
-		          <span>
-		            <Glyphicon glyph="search" className='btn-lg'/>
-		            <h3> Song Details</h3>
-		            <p> Now more about the songs </p>
-		          </span>
-		          
-		          <span>
-		              <Glyphicon glyph="time" className='btn-lg'/>
-		              <h3> Less Time </h3>
-		              <p> Get time to enjoy music </p>
-		          </span>
-		        
-		        </div>
-      		</div>
+
+		       	<div className= 'container'>
+			        <div className='counter'>
+						<CountUp start={0} end={1250} />
+						<h3> Songs </h3>
+					</div>
+					<div className='counter'>
+						<CountUp start={0} end={8000} />
+						<h3> Users </h3>
+					</div>
+					<div className='counter'>
+						<CountUp start={0} end={100} />
+						<h3> Shares </h3>
+					</div>
+					<div className='counter'>
+						<CountUp start={0} end={100} />
+						<h3> Happy Moments </h3>
+					</div>
+				</div>
+		    </div>
 		)
 	}
 }
