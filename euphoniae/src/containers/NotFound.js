486b2270fb07cbbc374f5e2b07aa12f48a195355
Nuno Pedro Ribeiro Gago <nprgago@gmail.com>
2018-05-23 11:32:42 +0100

    feat: add 404 page and styles, and set route

---
+++ euphoniae/src/containers/NotFound.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Glyphicon } from 'react-bootstrap';
+import notfound from '../img/404.svg'; 
+import "../scss/NotFound.scss";
+
+export default () =>
+  <div className="NotFound">
+    
+    <div className='error-message'>
+    	
+    	<h1>Oops</h1>
+    	<h2> We can't seem to fund the page you're looking for</h2>
+    	<h3> Error code: 404</h3>
+    	<p> Here are some helpful links instead </p>
+    	<ul>
+    		
+    		<LinkContainer to='./'>
+	    		<li> 
+	    			<Glyphicon glyph="home" className='btn-lg'/>
+	    			<span> Home </span> 
+	    		</li>
+    		</LinkContainer>
+    		
+    		<LinkContainer to='/login'>
+	    		<li> 
+	    			<Glyphicon glyph="user" className='btn-lg'/>
+	    			<span> Login </span> 
+	    		</li>
+    		</LinkContainer>
+    		
+    		<LinkContainer to='/signup'>
+    		<li> 
+    			<Glyphicon glyph="pencil" className='btn-lg'/>
+    			<span> Signup </span> 
+    		</li>
+    		</LinkContainer>
+
+    	</ul>
+
+    </div>
+
+    <div className='error-image'>
+		<img alt='404 not found' src={notfound}/>
+    </div>	
+  	
+  </div>;
